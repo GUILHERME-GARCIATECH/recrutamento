87c3b7f077e204816e3b56308ad3d2987ee0d7bf
Guilherme Garcia <guilherme.garciatech@gmail.com>
2026-04-22 12:45:02 -0400

    atualização

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 let bancoDeCandidatos = [];
 
-document.getElementById('formCandidato').addEventListener('submit', function(evento) {
+document.getElementById('formCandidato').addEventListener('submit', function (evento) {
     evento.preventDefault(); // Impede o envio padrão e o recarregamento da página
-    
+
     const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senioriedade = document.getElementById('senioridade').value;
+    const tmpExperiencia = document.getElementById('tmpExperiencia').value;
+    const portifolio = document.getElementById('portifolio').value;
+
+    const checkboxesMarcados = document.querySelectorAll('input[name="stack"]:checked'); //Pegas a chackbox marcadas
+
+    const tecnologias = Array.from(checkboxesMarcados).map(checkbox => checkbox.value); //Transforma em um array e pega somente os valores de cada elemento.
+
+
 
     const novoCandidato = {
         nome: nome,
-        tecnologia: tecnologia,
+        email: email,
+        senioridade: senioridade,
+        tmpExperiencia: tmpExperiencia,
+        portifolio: portifolio,
+        tecnologias: tecnologias
+
     }
-    bancoDeCandidatos.push()
+    bancoDeCandidatos.push(novoCandidato);
     this.reset();
+    console.log(bancoDeCandidatos);
 });
--- assets/functions.js
+++
@@ -1,3 +0,0 @@
-function botao_enviar(){
-    
-}
