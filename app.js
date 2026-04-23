25984d247fc7bdb8305105f6a3e41b41f5cd09f6
Guilherme Garcia <guilherme.garciatech@gmail.com>
2026-04-22 21:49:39 -0400

    aplicando funcionaliades nos botões

--- app.js
+++ app.js
@@ -13,18 +13,67 @@ document.getElementById('formCandidato').addEventListener('submit', function (ev
 
     const tecnologias = Array.from(checkboxesMarcados).map(checkbox => checkbox.value); //Transforma em um array e pega somente os valores de cada elemento.
 
-
-
     const novoCandidato = {
         nome: nome,
         email: email,
         senioridade: senioridade,
-        tmpExperiencia: tmpExperiencia,
+        tmpExperiencia: Number(tmpExperiencia),
         portifolio: portifolio,
         tecnologias: tecnologias
-
     }
+
     bancoDeCandidatos.push(novoCandidato);
     this.reset();
     console.log(bancoDeCandidatos);
 });
+
+let res = document.getElementById('resultado');
+function listartodos() {
+    res.innerHTML = ''
+
+    if (bancoDeCandidatos.length == 0) {
+        res.innerHTML = `<p>Nenhum candidato cadastrado!</p>`
+        return;
+    }
+
+    res.innerHTML = bancoDeCandidatos.map(candidato => `<div>
+    <h3>${candidato.nome}</h3>
+    <p>Email: ${candidato.email}</p>
+    <p>Senioridade: ${candidato.senioridade}</p>
+    <p>Tempo de experiência: ${candidato.tmpExperiencia} anos</p>
+    <p>Link do Portifolio: ${candidato.portifolio}</p>
+    <p>Stacks: ${candidato.tecnologias.join(', ')}</p>
+    </div>`).join('');
+}
+
+function filtarSeniores() {
+    res.innerHTML = '';
+
+    const experientes = bancoDeCandidatos.filter(c => c.tmpExperiencia > 5);
+
+    if (experientes.length == 0) {
+        res.innerHTML = `<p>Nenhum candidato tem a experiência necessária!</p>`
+        return;
+    }
+
+    res.innerHTML = experientes.map(ex => `<div>
+    <h3>EXPERIENTES</h3>
+    <p>Nome: ${ex.nome}<p>
+    <p>Tempo de experiência: ${ex.tmpExperiencia}<p>
+    <p>Portifolio: ${ex.portifolio}</p>
+    </div>`).join('');
+}
+
+function calcularMediaXP() {
+    res.innerHTML = '';
+
+    if (bancoDeCandidatos.length == 0) {
+        res.innerHTML = `<p>Nenhum candidato cadastrado!</p>`
+        return;
+    }
+    
+    const soma = bancoDeCandidatos.reduce((acc, cur) => acc + cur.tmpExperiencia, 0);
+    const mediaEX = bancoDeCandidatos.length > 0 ? soma / bancoDeCandidatos.length : 0;
+
+    res.innerHTML = `<p>A média de experiência dos candidato é ${mediaEX.toFixed(2)}</p>`
+}
